a0cb88efb314c85824e07fdf263ab9caa9e58771
HM <hieumdd@gmail.com>
2021-09-25 22:08:03 +0700

    !feat: Refactored tests

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-import json
-import base64
-
 from models.models import Taboola
-from broadcast import broadcast
-
 
 def main(request):
-    request_json = request.get_json(silent=True)
-    message = request_json["message"]
-    data_bytes = message["data"]
-    data = json.loads(base64.b64decode(data_bytes).decode("utf-8"))
+    data = request.get_json(silent=True)
     print(data)
 
-    if "broadcast" in data:
-        results = broadcast(data)
-    elif "table" in data:
-        results = Taboola.factory(
+    if "table" in data:
+        response = Taboola.factory(
             table=data["table"],
             start=data.get("start"),
             end=data.get("end"),
         ).run()
     else:
-        raise NotImplementedError(data)
+        raise ValueError(data)
 
-    responses = {"pipelines": "Taboola", "results": results}
-    print(responses)
-    return responses
+    print(response)
+    return response
---
+++ test/test_units.py
@@ -0,0 +1,38 @@
+import pytest
+
+from unittest.mock import Mock
+
+from main import main
+
+
+@pytest.mark.parametrize(
+    "table",
+    [
+        "TopCampaignContent",
+        "CampaignSummary",
+        "CampaignSummaryHourly",
+    ],
+)
+@pytest.mark.parametrize(
+    (
+        "start",
+        "end",
+    ),
+    [
+        (None, None),
+        ("2021-08-20", "2021-09-20"),
+    ],
+    ids=[
+        "auto",
+        "manual",
+    ],
+)
+def test_pipelines(table, start, end):
+    data = {
+        "table": table,
+        "start": start,
+        "end": end,
+    }
+    req = Mock(get_json=Mock(return_value=data), args=data)
+    res = main(req)
+    assert res["num_processed"] == res["output_rows"]
